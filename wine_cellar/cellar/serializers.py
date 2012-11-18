7a3cfb57e9e6a3f4cc32f916ea1fa5290608cb77
Jeff Sheffield <jeff.sheffield@gmail.com>
2012-11-18 17:52:21 -0600

    movement a-foot

---
+++ wine_cellar/cellar/serializers.py
@@ -0,0 +1,9 @@
+from django.forms import widgets
+from rest_framework import serializers
+from cellar import models
+
+class WineSerializer( serializers.ModelSerializer ):
+    """ Here we take advantage of the ModelSerializer ( analogous to ModelForm ) """
+    class Meta:
+        model = models.Wine
+        fields = ( 'id', 'name', 'year', 'grapes', 'country', 'region', 'description', 'picture' )
--- wine_cellar/wine_cellar/settings.py
+++ wine_cellar/wine_cellar/settings.py
@@ -73,8 +73,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_extensions',
     'cellar',
-    
 )
 
 LOGGING = {
